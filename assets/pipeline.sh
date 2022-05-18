fdc192d0f4b547b1890d78a7e383c0304139ea1d
xl355 <xl355@lab-rg06-01.cms.waikato.ac.nz>
2022-05-18 19:16:31 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,3 +1,10 @@
+if ["$1" == ""];then
+    echo "enter commit message"
+    exit
+else
+    echo "run"
+fi
+
 echo "<1> prepare npm"
 if npm install; then
 	echo "install successful"
@@ -16,7 +23,17 @@ fi
 
 echo "<3>start application"
 if npm run start; then
+        echo "success"
 else
 	echo "run failed"
 	exit
 fi
+
+echo "<git add .>"
+git add .
+
+echo "<git commit -m message>"
+git commit -m "$1"
+
+echo "<git push>"
+git push origin master
